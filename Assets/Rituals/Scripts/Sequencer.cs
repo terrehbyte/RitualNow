0df37d3588b3a02ed6be6e4bfad464f68bd7df45
Terry Nguyen <terreh@terrehbyte.com>
2016-01-31 15:25:21 -0800

    Temporary sequencer added, ramps up production three times

--- Assets/Rituals/Scripts/Sequencer.cs
+++ Assets/Rituals/Scripts/Sequencer.cs
@@ -3,5 +3,46 @@ using System.Collections;
 
 public class Sequencer : MonoBehaviour
 {
-    
+    private float Accumulator;
+    public float Step = 15.0f;
+
+    public int MaxRamps = 3;
+
+    public AssemblyLine assem;
+    public float RampUpSpawnRate = 0.1f;
+    public float RampUpParcelSpeed = 0.7f;
+
+    public float TimeBetweenRamp = 7.0f;
+
+    void Update()
+    {
+        if (!GameMode.instance.isGameActive)
+            return;
+
+        if (MaxRamps <= 0)
+            return;
+
+        Accumulator += Time.deltaTime;
+
+        if (Accumulator > Step)
+        {
+            // uncomment for hilarity
+            Accumulator -= Step;
+
+            RampUpProduction();
+        }
+    }
+
+    public void RampUpProduction()
+    {
+        Debug.Log("Work harder!");
+
+        MaxRamps -= 1;
+
+        Accumulator -= TimeBetweenRamp;
+        assem.addlDelay += TimeBetweenRamp;
+
+        assem.SpawnInterval -= RampUpSpawnRate;
+        assem.Speed += RampUpParcelSpeed;
+    }
 }
